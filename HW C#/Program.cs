955d6b5d44e527f45b0fd3d4af373d764d832ba2
Mikita <nikita_popow_1993@mail.ru>
2023-02-13 12:45:26 +0300

    task - done

--- HW C#/Program.cs
+++ HW C#/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// функция печати элементов массива
+void PrintArr<T>(T[] arr)
+{
+    Console.Write("Cгенерированный массив: ");
+    foreach (T i in arr)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Введите строку содержащую элементы массива: ");
+string strElements = Console.ReadLine();
+
+//функция поиска элементов длинной больше заданного числа
+string[] LenSubString(int len, string str)
+{
+    string[] s = str.Split(' ');
+    if (s.Length == 0)
+    {
+        string[] res = new string[0];
+        return res;
+    }
+    else
+    {
+        string result = "";
+        foreach (string item in s)
+        {
+            if (item.Length >= len)
+            {
+                result += item + " ";
+            }
+        }
+        string[] resultArr = result.Split(' ');
+        return resultArr;
+    }
+}
+
+string[]resultArr = LenSubString(3, strElements);
+PrintArr(resultArr);
+
+//вариант 2
+string[] LenSubString2(int len, string str)
+{
+    string[] itemsArr = str.Split(' ');
+    string[] resultArr = itemsArr.Where(substring => substring.Length >= 3).ToArray();
+    return resultArr;
+}
+
+Console.WriteLine();
+string[]resultArr2 = LenSubString2(3, strElements);
+PrintArr(resultArr2);
